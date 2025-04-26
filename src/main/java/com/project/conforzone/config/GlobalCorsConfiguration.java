c423285e22acd27c4d86c418200d676b7511fdf5
Daniel Gómez <danigb2904@gmail.com>
2025-04-27 01:41:19 +0200

    corsLink added

--- src/main/java/com/project/conforzone/config/GlobalCorsConfiguration.java
+++ src/main/java/com/project/conforzone/config/GlobalCorsConfiguration.java
@@ -1,5 +1,6 @@
 package com.project.conforzone.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,10 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class GlobalCorsConfiguration implements WebMvcConfigurer {
+    @Value("${CORS_LINK}")
+    private String corsLink;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(corsLink)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
